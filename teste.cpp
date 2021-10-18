f46e156ffc239d27c5e4e38546ae17a32102cfd4
MarcosSilva13 <marcosslp45@gmail.com>
2021-10-18 15:30:19 -0300

    adicionando arquivo

---
+++ teste.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+ 
+using namespace std;
+ 
+int main() {
+  UINT CPAGE_UTF8 = 65001;
+  UINT CPAGE_DEFAULT = GetConsoleOutputCP();
+  SetConsoleOutputCP(CPAGE_UTF8);
+ 
+  cout << "Olá, seja bem-vindo!"; 
+ 
+  cout << endl << endl;
+  system("pause");
+  return 0;
+}
